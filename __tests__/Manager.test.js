107449d8b9ef98519a3185d83a808a1b80f963e9
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2023-02-08 23:08:20 -0500

    adding manger test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,26 @@
-nothing
+const Managers = require('../lib/Managers.js');
+
+
+const testObject = {name: 'Man', id: 1, email: "man@some.com", officeNumber: "777"}
+
+test('Checks for inherited Employee properties', () => {
+    const manager = new Managers(testObject);
+    expect(manager.name).toEqual(expect.any(String));
+    console.log(`Manager name: ${manager.name}`)
+    expect(manager.id).toEqual(expect.any(Number));
+    console.log(`Manager id: ${manager.id}`)
+    expect(manager.email).toEqual(expect.any(String));
+    console.log(`Manager email: ${manager.email}`)
+});
+
+test('Checks for office number', () => {
+    const manager = new Managers(testObject);
+    expect(manager.officeNumber).toEqual(expect.any(String));
+    console.log(`Manager office number: ${manager.officeNumber}`)
+});
+
+test('Gets Manager role', () => {
+    const manager = new Managers(testObject);
+    expect(manager.getRole()).toBe('Managers');
+    console.log(`Manager getRole(): ${manager.getRole()}`)
+});
